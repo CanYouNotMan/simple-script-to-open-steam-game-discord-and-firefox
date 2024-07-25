43a7d3ff5a56dd02115d24eac5fb871a3fb2b5c2
CanYouNotMan <nickha401@gmail.com>
2024-07-24 19:14:15 -0700

    Add files via upload

---
+++ opening personal startup apps.py
@@ -0,0 +1,20 @@
+#code to turn on steam, dc, and firefox
+import os
+
+#open firefox
+os.startfile(r"C:\Users\nickh\AppData\Local\Mozilla Firefox\firefox.exe")
+
+#open forza horizon 4
+os.startfile(r"D:\Steam\steamapps\common\ForzaHorizon4\ForzaHorizon4.exe")
+
+#open discord
+os.startfile(r"C:\Users\nickh\OneDrive\Desktop\Discord.lnk")
+
+
+
+
+
+
+
+
+
