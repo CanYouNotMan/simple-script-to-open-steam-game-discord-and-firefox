bc65f383a906d2df25ae85b2863d540c120601cf
CanYouNotMan <nickha401@gmail.com>
2024-07-24 20:54:45 -0700

    Add files via upload

--- opening personal startup apps.py
+++ opening personal startup apps.py
@@ -1,15 +1,36 @@
 #code to turn on steam, dc, and firefox
 import os
+import pyautogui
+import time
 
-#open firefox
+print("Starting script...")
+
+# Move the mouse to open Forza Horizon 4
+print("Opening Forza Horizon 4...")
+pyautogui.click(200, 50)
+pyautogui.click(200, 50)
+time.sleep(53)
+
+# Start up the menu
+print("Pressing enter to start the menu...")
+pyautogui.press('enter')
+time.sleep(8)
+print ("pressing enter to load in my save profile")
+pyautogui.press('enter')
+
+# Open Firefox
+
+
+print("Opening Firefox...")
 os.startfile(r"C:\Users\nickh\AppData\Local\Mozilla Firefox\firefox.exe")
+time.sleep(5)
 
-#open forza horizon 4
-os.startfile(r"D:\Steam\steamapps\common\ForzaHorizon4\ForzaHorizon4.exe")
-
-#open discord
+# Open Discord
+print("Opening Discord...")
 os.startfile(r"C:\Users\nickh\OneDrive\Desktop\Discord.lnk")
+time.sleep(5)
 
+print("Script finished.")
 
 
 
